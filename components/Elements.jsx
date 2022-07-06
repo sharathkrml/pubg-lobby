db02fefd50351a442a2305b9b06cb4be5146b8ad
Sharath <sharathkrml@gmail.com>
2022-07-06 13:21:56 +0530

    added texture

--- components/Elements.jsx
+++ components/Elements.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { Euler, SpotLightHelper } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 import Male from "./Male";
 import Car from "./Car";
 import { degToRad } from "three/src/math/MathUtils";
@@ -9,31 +10,51 @@ import {
   OrbitControls,
   PerspectiveCamera,
   useHelper,
+  useTexture,
 } from "@react-three/drei";
 
 const Elements = () => {
-  const spotLight = useRef(null);
-  useHelper(spotLight, SpotLightHelper, "red");
+  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] =
+    useTexture([
+      "Grass/Color.jpg",
+      "Grass/Displacement.jpg",
+      "Grass/Normal.jpg",
+      "Grass/Roughness.jpg",
+      "Grass/AmbientOcclusion.jpg",
+    ]);
+  const sky = useTexture(["sky.jpg"]);
   return (
     <>
       <PerspectiveCamera makeDefault position={[0, 3, 16]} />
       <OrbitControls />
-
-      <Male position={[0, 2.55, 5]} castShadow />
+      <Male position={[0, 3.2, 5]} castShadow />
 
       <ambientLight color="#ffffff" />
-
-      <spotLight
-        castShadow
-        position={[-9, 8, 10]}
-        ref={spotLight}
-        angle={1}
-        distance={20}
-      />
       <mesh rotation={[-degToRad(90), 0, 0]} receiveShadow>
-        <planeGeometry args={[100, 100]} />
-        <meshStandardMaterial color="#1ea8d8"></meshStandardMaterial>
+        <planeGeometry args={[100, 100, 10, 10]} />
+        <meshStandardMaterial
+          displacementScale={10}
+          map={colorMap}
+          displacementMap={displacementMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          roughness={10}
+          metalness={0.2}
+          //   wireframe
+          aoMap={aoMap}
+        />
       </mesh>
+      <Environment background>
+        <mesh scale={100}>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshBasicMaterial
+            // wireframe
+            displacementScale={0}
+            side={BackSide}
+            map={sky[0]}
+          />
+        </mesh>
+      </Environment>
     </>
   );
 };
---
+++ components/Texture.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Texture = () => {
+  return <div>Texture</div>;
+};
+
+export default Texture;
